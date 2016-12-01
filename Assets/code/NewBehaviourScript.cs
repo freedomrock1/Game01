9e59a2b19eb63030e41494680a4fa6675860f253
Chad Frederick <freedomrock1@gmail.com>
2016-11-30 19:04:58 -0500

    Init
    Initial code

---
+++ Assets/code/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {     
+            
+            Debug.Log(Input.mousePosition+" "+this.GetType());
+            transform.position = new Vector3(0, 7, 0);
+        }
+	}
+}
